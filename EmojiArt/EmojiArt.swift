060bf1bd6131a50fcf17e535de6d226e0a3ec3e5
David <genglanghuang@gmail.com>
2020-06-24 23:34:28 +0100

    Finished the drag function

---
+++ EmojiArt/EmojiArt.swift
@@ -0,0 +1,36 @@
+//
+//  EmojiArt.swift
+//  EmojiArt
+//
+//  Created by Suica on 24/06/2020.
+//  Copyright © 2020 Suica. All rights reserved.
+//
+
+import Foundation
+
+struct  EmojiArt {
+    var backgroundURL: URL?
+    var emojis = [Emoji]()
+    
+    struct Emoji: Identifiable {
+        let text: String
+        var x: Int // offset from center
+        var y: Int // offset from center
+        var size: Int
+        let  id: Int
+        
+        fileprivate init(text: String, x: Int, y: Int, size: Int, id: Int) {
+            self.text = text
+            self.x = x
+            self.y = y
+            self.size = size
+            self.id = id
+        }
+    }
+    
+    private var uniqueEmojiId = 0
+    mutating func addEmoji(_ text: String, x: Int, y: Int, size: Int) {
+        uniqueEmojiId += 1
+        emojis.append(Emoji(text: text, x: x, y: y, size: size, id: uniqueEmojiId))
+    }
+}
